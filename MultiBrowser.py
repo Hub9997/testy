a89ab13be81d7870aacd594a23f58df885252736
Hub9997 <hrubetex@gmail.com>
2021-05-23 09:28:45 +0200

    refactor

--- MultiBrowser.py
+++ MultiBrowser.py
@@ -1,14 +1,13 @@
-:from selenium import  webdriver
+:from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-#driver = webdriver.Chrome(executable_path = "C:\Program Files\chromedriver.exe")
-driver = webdriver.Firefox(executable_path = "C:\Program Files\geckodriver.exe")
-#driver = webdriver.Ie(executable_path = "C:\Program Files\IEDriverServer.exe")
+# driver = webdriver.Chrome(executable_path = "C:\Program Files\chromedriver.exe")
+driver = webdriver.Firefox(executable_path="C:\Program Files\geckodriver.exe")
+# driver = webdriver.Ie(executable_path = "C:\Program Files\IEDriverServer.exe")
 driver.get("http://newtours.demoaut.com/")
 
-print(driver.title)  #Title of the page
-print(driver.current_url) #returns the URL of the page
-print(driver.page_source) # HTML code for the page
-
+print(driver.title)  # Title of the page
+print(driver.current_url)  # returns the URL of the page
+print(driver.page_source)  # HTML code for the page
 
 driver.close()
